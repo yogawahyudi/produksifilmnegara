aadf5985e35ee89510b0f5eb27214c9d5dfe3cc2
yoga wahyudi <yogawhy8@gmail.com>
2022-07-27 22:25:15 +0700

    fixed assets loaded in login form

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @php
+    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+@endphp
+<script type="module" src="{{asset('build/'.$manifest['resources/js/app.js']['file'])}}"></script>
+<link rel="stylesheet" href="{{asset('build/'.$manifest['resources/css/app.css']['file'])}}">
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
